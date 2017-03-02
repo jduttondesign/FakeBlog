575383a4775bd3fed5fa0b63de52b3259d93c1bf
Jennifer Dutton <jduttondesign@gmail.com>
2017-03-01 23:10:05 -0600

    initial commit

---
+++ FakeBlog/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        public
+    }
+}
